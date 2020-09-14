7520d0e21f31f817ff49ba8c7a49a63cb1b89543
Aleksi Hirvonen <aleksi.hirvonen@tuni.fi>
2020-09-14 16:09:54 +0300

    Add start:test to package.json, return populated json when updating blog

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -64,7 +64,7 @@ blogsRouter.put('/:id', async(request, response)=> {
             url: body.url,
             likes: body.likes
         }
-        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true}).populate('user', {username: 1, name: 1})
         response.json(updatedBlog.toJSON())
     } else {
         response.status(400).end()
